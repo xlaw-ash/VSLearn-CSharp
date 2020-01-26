f401655bb48ddeda6443fdaeddd0fc5fffe1c0ad
ash <59516869+xlaw-ash@users.noreply.github.com>
2020-01-26 09:58:11 +0530

    Update Program.cs to initialize SeedData

--- RazorPagesMovieDB/Program.cs
+++ RazorPagesMovieDB/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RazorPagesMovieDB.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace RazorPagesMovieDB
 {
@@ -13,7 +15,23 @@ namespace RazorPagesMovieDB
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch(Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occured in Seeding DB");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
